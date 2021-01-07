cfdf1d867298a605abd52fe69ea9b6990f14b0f2
Atharva Munshi <atharvachaitanya.munshi@sjsu.edu>
2021-01-07 15:58:28 +0530

    Header component

---
+++ fb-clone/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header';
+function App() {
+  return (
+    //BEM naming convention-> Helps build app and scale up
+    <div className="app">
+      <Header />
+      {/* App body* /}
+        {/* Sidebar */}
+        {/* Feed*/}
+        {/* Widgets*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ fb-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB27WZWY7GsgWjO4HJ9RdFCbq7e_0Hq5Sk",
+    authDomain: "facebook-clone-651e3.firebaseapp.com",
+    projectId: "facebook-clone-651e3",
+    storageBucket: "facebook-clone-651e3.appspot.com",
+    messagingSenderId: "213399204102",
+    appId: "1:213399204102:web:a4259c368f4ecd0ac92e87",
+    measurementId: "G-BRT0G6X2HP"
+  };
